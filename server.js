a08ef3410be90f1caf92cd551dcf5dd7fc7df66c
AMIT KUMAR <amitk.ug19.cs@nitp.ac.in>
2022-05-27 10:41:16 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+//import express server
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const expressLayout = require('express-ejs-layouts')
+const path = require('path')
+
+app.get('/', function(req,res){
+    res.render('home')
+})
+
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine', 'ejs')
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
