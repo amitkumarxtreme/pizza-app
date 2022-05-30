cc52f8d37aff12f59d100e43c39aa116f8cfb0d8
AMIT KUMAR <amitk.ug19.cs@nitp.ac.in>
2022-05-30 13:46:59 +0530

    cart added dynamically and session used

--- server.js
+++ server.js
@@ -1,33 +1,53 @@
 //import express server
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
 const path = require('path')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDBStore = require('connect-mongo')
+
+// Database connection
+mongoose.connect("mongodb://localhost:27017/pizza" , {useNewUrlParser:true});
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database connected...');
+});
+
+// session store
+
+// session config
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: MongoDBStore.create({
+        mongoUrl: "mongodb://localhost:27017/pizza"
+    }),
+    saveUninitialized: false,
+    cookie: { maxAge: 1000*60*60*24 } // 24 hours
+}))
+
+app.use(flash())
 
 //Assets
 app.use(express.static('public'))
+app.use(express.json())
 
+// Global middleware
+app.use((req,res,next) => {
+    res.locals.session = req.session
+    next()
+})
 
+// set template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
-app.get('/', function(req,res){
-    res.render('home')
-})
-
-app.get('/cart', (req,res) => {
-    res.render('customers/cart');
-})
-
-app.get('/login', (req,res) => {
-    res.render('auth/login');
-})
-
-app.get('/register', (req,res) => {
-    res.render('auth/register');
-})
+require('./routes/web')(app)
 
 
 const PORT = process.env.PORT || 3000
